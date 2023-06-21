22bb618a289c820c726146a7a1e6a5f4e55d9f7d
BibekM <bibekmbkb@gmail.com>
2023-06-21 10:29:31 -0700

    implemented local storage

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-
-function loadSavedWorkflow(){
-    
-}
+var dayData;
 
 function setColors(){
     var currTime = dayjs();
@@ -17,10 +14,43 @@ function setColors(){
     }
 }
 
-function saveClick(){
+function loadSavedWorkflow(){
+    dayData = JSON.parse(window.localStorage.getItem("events"));//get from local storage
+    if(!dayData){//if dayData has not been established
+        dayData = {
+            9:"",
+            10:"",
+            11:"",
+            12:"",
+            13:"",
+            14:"",
+            15:"",
+            16:"",
+            17:"",
+        };
+        window.localStorage.setItem("events", JSON.stringify(dayData));
+    }
 
+    for(let i=9; i<18; i++){
+        $("#hour-" + i + " textarea").html(dayData[i]);
+    }
 }
 
+function saveClicked(event){
+    //grabs the data from html
+    var timeBlock = $(event.target).parent();//this gets the row of the clicked save btn
+    var eventValue = timeBlock.children("textarea").val();
+    var clickedHour = timeBlock.attr("id").split("-")[1];
+
+    //modifies the data and stores it to local storage
+    dayData[clickedHour] = eventValue;
+    window.localStorage.setItem("events", JSON.stringify(dayData));
+    event.preventDefault();
+}
+
+$(".saveBtn").on("click", saveClicked);
+
 $(function() {
+    loadSavedWorkflow();
     setColors();
 });
