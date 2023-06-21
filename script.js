c95c169f9d0654e15c661c4dd7de10b9743c7749
BibekM <bibekmbkb@gmail.com>
2023-06-21 10:31:35 -0700

    completed save btn functionality and local storage is loading and saving correctly. Need to work on css formatting and maybe animations.

--- script.js
+++ script.js
@@ -31,22 +31,22 @@ function loadSavedWorkflow(){
         window.localStorage.setItem("events", JSON.stringify(dayData));
     }
 
-    // for(let i=9; i<18; i++){
-    //     $("#hour-" + i + " textarea").html(dayData[i]);
-    // }
+    for(let i=9; i<18; i++){
+        $("#hour-" + i + " textarea").html(dayData[i]);
+    }
 }
 
-// function saveClicked(event){
-//     //grabs the data from html
-//     var timeBlock = $(event.target).parent();//this gets the row of the clicked save btn
-//     var eventValue = timeBlock.children("textarea").val();
-//     var clickedHour = timeBlock.attr("id").split("-")[1];
+function saveClicked(event){
+    //grabs the data from html
+    var timeBlock = $(event.target).parent();//this gets the row of the clicked save btn
+    var eventValue = timeBlock.children("textarea").val();
+    var clickedHour = timeBlock.attr("id").split("-")[1];
 
-//     //modifies the data and stores it to local storage
-//     dayData[clickedHour] = eventValue;
-//     window.localStorage.setItem("events", JSON.stringify(dayData));
-//     event.preventDefault();
-// }
+    //modifies the data and stores it to local storage
+    dayData[clickedHour] = eventValue;
+    window.localStorage.setItem("events", JSON.stringify(dayData));
+    event.preventDefault();
+}
 
 $(".saveBtn").on("click", saveClicked);
 
